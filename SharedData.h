a701802c66a1d217a25bd888072b386ac1ca1d5d
Shachar Meir <shachar.meir007@gmail.com>
2020-01-09 18:52:01 +0200

    SharedData.h members added

--- SharedData.h
+++ SharedData.h
@@ -4,11 +4,14 @@
 
 #ifndef FLIGHT_SIMULATOR__SHAREDDATA_H_
 #define FLIGHT_SIMULATOR__SHAREDDATA_H_
+#include <vector>
+#include <string>
 //This class is for shared data the thread might need.
 //Every time you need to share data between threads you'll use
 //this class.
 class SharedData {
-
+  std::vector<std::string> vars_right_Bind;
+  std::vector<std::string> vars_left_Bind;
 };
 
 #endif //FLIGHT_SIMULATOR__SHAREDDATA_H_
